134dd663b40b34f0e9682ede6fd6be10ec7423d4
Adrian <ubudubu13@o2.pl>
2015-03-05 14:50:44 +0100

    Outstanding project
    problem 30 done

---
+++ Problem23/Problem23/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            List<int> numbers = new List<int>();
+            for (int i = 1; i < 28123; i++)
+            {
+                numbers.Add(i);
+            }
+
+            List<int> abudants = new List<int>();
+            for (int i = 12; i < 28123; i++)
+            {
+                if (IsAbundant(i)) abudants.Add(i);
+            }
+
+            Parallel.For(1, abudants.Count, i =>
+            {
+                Parallel.For(i, abudants.Count, j =>
+                {
+                    numbers.Remove(abudants.ElementAt(i) + abudants.ElementAt(j));
+                });
+            });
+       
+            sw.Stop();
+            Console.WriteLine(numbers.Sum());
+            Console.WriteLine("time: " + sw.ElapsedMilliseconds + " ms, " + sw.ElapsedTicks + " ticks.");
+            Console.Read();
+        }
+
+        public static Boolean IsAbundant(int number)
+        {
+            List<int> divisors = new List<int>();
+            for (int i = 1; i < number/2+1; i++)
+            {
+                if (number % i == 0) divisors.Add(i);
+            }
+            if (divisors.Sum() > number) return true;
+            else return false;
+        }
+    }
+}
---
+++ Problem25/Problem25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace Problem25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            List<BigInteger> fibSequence = new List<BigInteger>();
+            fibSequence.Add(1);
+            fibSequence.Add(1);
+            while(fibSequence.ElementAt(fibSequence.Count-1).ToString().Length<1000)
+            {
+                fibSequence.Add(fibSequence.ElementAt(fibSequence.Count - 1) + fibSequence.ElementAt(fibSequence.Count - 2));
+            }
+            sw.Stop();
+            Console.WriteLine(fibSequence.Count);
+            Console.WriteLine(fibSequence.ElementAt(fibSequence.Count-1));
+            Console.WriteLine("Time: " + sw.ElapsedMilliseconds + " ms.");
+            Console.Read();
+        }
+
+    }
+}
